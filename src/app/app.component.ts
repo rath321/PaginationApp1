e5f7f64450e2d3f8e391699de0c231e114cdb113
ETEC\NikhilRathore <nikhilrathore@policybazaar.com>
2021-12-07 14:24:48 +0530

    nihkhi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { RandomUserService } from './services/random-user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PaginationApp';
+export class AppComponent  {
+ data: Array<any>
+ page: number=1;
+ totalRecords:number | undefined;
+ constructor(private randomUser:RandomUserService){
+   this.data=new Array<any>()
+
+
+ }
+ submit(){
+   this.randomUser.getData().subscribe((data)=>{
+     this.data=data.results;
+     this.totalRecords=data.results.length;
+   })
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
